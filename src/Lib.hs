cf97296c439b100737bd40898f1effcdae1483ba
Ryan Breen <ryan@breen.xyz>
2018-09-01 20:30:42 -0400

    more lessons

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,6 +10,8 @@ getLesson = do
 runLesson :: Int -> IO ()
 runLesson 1 = lesson1
 runLesson 2 = lesson2
+runLesson 3 = lesson3
+runLesson 4 = lesson4
 runLesson _ = putStrLn "Lesson not found!"
 
 lesson1 :: IO ()
@@ -20,6 +22,34 @@ lesson1 = do
 
 lesson2 :: IO ()
 lesson2 = do
-    putStr "What is the input string? "
-    name <- getLine
+    name <- ask
     putStrLn (name ++ " has " ++ (show $ length name) ++ " characters.")
+    where
+        ask = do
+            putStr "What is the input string? "
+            inp <- getLine
+            case (length inp) of
+                0 -> do
+                    putStrLn "You must enter a value."
+                    ask
+                _ -> return inp
+
+lesson3 :: IO ()
+lesson3 = do
+    putStr "What is the quote? "
+    quote <- getLine
+    putStr "Who said it? "
+    person <- getLine
+    putStrLn $ person ++ " says, " ++ (show quote) 
+
+lesson4 :: IO ()
+lesson4 = do
+    noun <- ask "Enter a noun: "
+    verb <- ask "Enter a verb: "
+    adjective <- ask "Enter an adjective: "
+    adverb <- ask "Enter an adverb: "
+    putStrLn $ "Do you " ++ verb ++ " your " ++ noun ++ " " ++ adverb ++ "? That's " ++ adjective
+    where
+        ask question = do
+            putStr question
+            getLine
